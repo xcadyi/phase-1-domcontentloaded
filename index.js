735fa6c296f60ff4357d3e169f6fd2bd1ab79131
Selin Ayyildiz <xcadyix@gmail.com>
2022-09-15 14:39:06 -0400

    commit tree fa7ffbaaa2d5d8f7cf7a879a5ec97b974b75af50

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+  });
+  
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
+
+document.querySelector('p').innerHTML = "JavaScript is so cool. It lets me add text to my page programmatically."
